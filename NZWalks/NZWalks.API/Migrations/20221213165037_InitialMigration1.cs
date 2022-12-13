65b254d1fcce52a2ac3f140d50944e688bbe89c5
vaibhavpandey <vaibhav.pandey.nagpur@gmail.com>
2022-12-13 22:52:30 +0530

    second commit

---
+++ NZWalks/NZWalks.API/Migrations/20221213165037_InitialMigration1.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NZWalks.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Popoulation",
+                table: "Regions",
+                newName: "Population");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Population",
+                table: "Regions",
+                newName: "Popoulation");
+        }
+    }
+}
